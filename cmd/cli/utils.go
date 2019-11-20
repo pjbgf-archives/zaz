1408ddc5d9d9fc97605dcc19a47b09a87e10d839
Paulo Gomes <pjbgf@linux.com>
2019-11-20 07:41:17 +0000

    Sanitise filepath

---
+++ cmd/cli/utils.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"errors"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+func fileExists(fileName string) bool {
+	if _, err := os.Stat(fileName); err == nil {
+		return true
+	}
+
+	return false
+}
+
+func sanitiseFileName(input string) (string, error) {
+	p := filepath.Clean(input)
+	if !path.IsAbs(p) {
+		base, err := os.Getwd()
+		if err != nil {
+			return "", errors.New("error getting current folder")
+		}
+
+		return filepath.Join(base, p), nil
+	}
+	return p, nil
+}
